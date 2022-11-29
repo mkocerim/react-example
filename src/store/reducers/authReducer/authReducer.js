267395e08ecc2ca38bd540bc571a63c07d433c6c
mkocerim <mkocerim@gmail.com>
2022-11-29 18:38:17 +0300

    initial project

---
+++ src/store/reducers/authReducer/authReducer.js
@@ -0,0 +1,25 @@
+export const SET_TOKEN = "set_token";
+export const REMOVE_TOKEN = "remove_token";
+
+const initialState = {
+  token: null,
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_TOKEN:
+      return {
+        ...state,
+        token: action.payload.token,
+      };
+    case REMOVE_TOKEN:
+      return {
+        ...state,
+        token: null,
+      };
+    default:
+      return state;
+  }
+}
+
+export default reducer;
